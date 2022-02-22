b9a8dd7e2e4eb9c05d01e9721ed58d5276cff718
josephine chan <jc41405n@pace.edu>
2022-02-22 10:10:05 -0500

    ClassLab 4

--- CS122/src/classSamples/polymorphism/SearchingAndSorting/Player.java
+++ CS122/src/classSamples/polymorphism/SearchingAndSorting/Player.java
@@ -1,5 +1,39 @@
 package classSamples.polymorphism.SearchingAndSorting;
 
 public class Player {
+	private final String name;
+	private int XP;
+	
+	public Player(String n, int xp) {
+		name = n;
+		XP = xp;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getXP() {
+		return XP;
+	}
+	
+	public void setXP(int xp) {
+		XP = xp;
+	}
+	
+	public boolean equals(Object o) {
+		return (this.name.equals(((Player) o).getName()));
+	}
+	
+	public String toString() {
+		return "Name: " + name + "\nXP: " + XP;
+	}
+	
+	public int compareTo(Player p) {
+		int result = 0;
 
+		if (name.equals(p.getName()))
+			result = name.compareTo(p.getName());
+		return result;
+	}
 }
